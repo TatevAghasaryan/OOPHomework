c9fd7baed2eca885b4edc3093b851329c1496a41
TatevAghasaryan <tatevikaghasaryan@gmail.com>
2015-10-14 22:57:35 +0400

    Initial version

---
+++ FactoryDesignPattern/FactoryDesignPattern/Factory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public static class Factory
+    {
+        public static Specialty Get(int id)
+        {
+            switch(id)
+            {
+                case 0: return new Manager(); break;
+                case 1:
+                case 2: return new Driver(); break;
+                case 3:
+                default: return new Programmer(); break;
+            }
+        }
+    }
+}
---
+++ FactoryDesignPattern/FactoryDesignPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                Specialty specialty = Factory.Get(i);
+                Console.WriteLine("Id = {0} Specialty = {1}",i,specialty.Title);
+
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
