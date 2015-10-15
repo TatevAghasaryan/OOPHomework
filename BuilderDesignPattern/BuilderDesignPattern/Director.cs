58df453a001a7aa456c539ec4c2e844c2694835e
TatevAghasaryan <tatevikaghasaryan@gmail.com>
2015-10-16 01:21:02 +0400

    Final version

---
+++ BuilderDesignPattern/BuilderDesignPattern/Director.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPattern
+{
+    public class Director
+    {
+        public void ConstructCar(IBuilder build)
+        {
+            build.CreateCar();
+        }
+    }
+}
---
+++ BuilderDesignPattern/BuilderDesignPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carType = Console.ReadLine();
+
+            if (carType == "BMW")
+            {
+                Director car = new Director();
+                IBuilder build = new BMW();
+                car.ConstructCar(build);
+            }
+
+            if (carType == "Mercedes")
+            {
+                Director car = new Director();
+                IBuilder build = new Mercedes();
+                car.ConstructCar(build);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
